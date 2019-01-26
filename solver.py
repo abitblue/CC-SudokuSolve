fcb4a69c626056200ce930a2d8a915770a3b76b7
resbit <ryang1453@gmail.com>
2019-01-26 18:22:10 -0500

    Began work on recursive backtracking
    Signed-off-by: resbit <ryang1453@gmail.com>

--- solver.py
+++ solver.py
@@ -1,6 +1,7 @@
 # Created by Ryan Yang
-
-# Global Variables:
+import time
+import copy
+import pprint
 
 # Easy
 grid1 = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
@@ -9,6 +10,7 @@ grid2 = '02081074070000310009000280500904008740020800316003020030270006000560000
 # Hard
 grid3 = '38.6.......9.......2..3.51......5....3..1..6....4......17.5..8.......9.......7.32'
 
+# Global Variables
 grid = []
 
 
@@ -24,17 +26,33 @@ cell_indexes = str_cross(rows, cols)
 
 def main():
     print("Sudoku Solver v1")
+    start_time = time.time()
 
     solve_grid = grid2
 
     print('Solving grid:', solve_grid, "\n\n")
     parse(solve_grid)
 
-    while not is_complete(grid):
-        check_values(grid)
-        print(grid)
+    #elimination(grid)
+    search(grid)
 
+    end_time = time.time()
     display(grid)
+
+    print("Grid solved in", (end_time - start_time), "seconds.")
+    #pprint.pprint(cycles)
+
+
+def get_set(index):
+    if index in str_cross('ABC', '123'): return '1'
+    elif index in str_cross('ABC', '456'): return '2'
+    elif index in str_cross('ABC', '789'): return '3'
+    elif index in str_cross('DEF', '123'): return '4'
+    elif index in str_cross('DEF', '456'): return '5'
+    elif index in str_cross('DEF', '789'): return '6'
+    elif index in str_cross('GHI', '123'): return '7'
+    elif index in str_cross('GHI', '456'): return '8'
+    elif index in str_cross('GHI', '789'): return '9'
 
 
 def parse(values):
@@ -54,11 +72,46 @@ def parse(values):
             new_cell["possible_values"] = [digit for digit in digits]
 
         grid.append(new_cell)
+
+    constrain(grid)
+
     display(grid)
-    print('\nParsed Grid:', grid)
+    print("Parsed Grid:")
+    print(grid)
 
 
-def check_values(a_grid):
+def num_incomplete(a_grid):
+    incomplete = 0
+    for item in a_grid:
+        if item["value"] is '0':
+            incomplete = incomplete + 1
+    return incomplete
+
+
+def display(a_grid):
+    print('\n')
+
+    for cell in a_grid:
+        try:
+            next_cell_row = a_grid[(a_grid.index(cell))+1]["index"][:1]
+        except IndexError:
+            pass
+
+        current_cell_row = cell["index"][:1]
+        col = cell["index"][1:]
+        value = (cell["value"])
+
+        if next_cell_row is 'I' and current_cell_row is 'A':
+            next_cell_row = 'A'
+
+        print(''.join(value).center(2) + ('| ' if col in '36' else '')\
+              + ('\n' if next_cell_row is not current_cell_row else '')\
+              + ('------+-------+------ \n' if (next_cell_row is not current_cell_row) and \
+                                            (next_cell_row in 'DG') else ''), end='')
+    print('\n')
+
+
+def constrain(a_grid):
     for cell in a_grid:
         if cell["value"] is not '0':
             row = cell["index"][:1]
@@ -90,53 +143,66 @@ def check_values(a_grid):
                     except ValueError:
                         pass
 
-        if len(cell["possible_values"]) == 1 and cell["value"] is '0':
-            cell["value"] = str(cell["possible_values"][0])
-            cell["possible_values"] = ['']
+
+def elimination(a_grid):
+    constrain(a_grid)
+
+    previous_cycle = 0
+    current_cycle = 1
+
+    while previous_cycle is not current_cycle:
+        previous_cycle = num_incomplete(a_grid)
+        for cell in a_grid:
+            if len(cell["possible_values"]) is 1 and cell["value"] is '0' and cell["possible_values"][0] is not '':
+                cell["value"] = str(cell["possible_values"][0])
+                cell["possible_values"] = ['']
+        current_cycle = num_incomplete(a_grid)
+        print(previous_cycle, current_cycle, a_grid)
 
 
-def is_complete(a_grid):
-    num_incomplete = 0
-    for item in a_grid:
-        if item["value"] is '0':
-            num_incomplete = num_incomplete + 1
-    if num_incomplete == 0:
-        return True
-    else:
-        print(num_incomplete)
-        return False
+def is_safe(a_grid, try_index, value):
+    safe = False
 
+    row = a_grid[try_index]["index"][:1]
+    col = a_grid[try_index]["index"][1:]
+    set_num = a_grid[try_index]["set"]
 
-def get_set(index):
-    if index in str_cross('ABC', '123'): return '1'
-    elif index in str_cross('ABC', '456'): return '2'
-    elif index in str_cross('ABC', '789'): return '3'
-    elif index in str_cross('DEF', '123'): return '4'
-    elif index in str_cross('DEF', '456'): return '5'
-    elif index in str_cross('DEF', '789'): return '6'
-    elif index in str_cross('GHI', '123'): return '7'
-    elif index in str_cross('GHI', '456'): return '8'
-    elif index in str_cross('GHI', '789'): return '9'
-
-
-def display(a_grid):
+    # Check used in row, col, or set
     for cell in a_grid:
-        try:
-            next_cell_row = a_grid[(a_grid.index(cell))+1]["index"][:1]
-        except IndexError:
-            pass
+        if row in cell["index"] and value in cell["value"]:
+            safe = False
+        elif col in cell["index"] and value in cell["value"]:
+            safe = False
+        elif set_num in cell["set"] and value in cell["value"]:
+            safe = False
+        else:
+            safe = True
 
-        current_cell_row = cell["index"][:1]
-        col = cell["index"][1:]
-        value = (cell["value"])
+    return safe
 
-        if next_cell_row is 'I' and current_cell_row is 'A':
-            next_cell_row = 'A'
 
-        print(''.join(value).center(2) + ('| ' if col in '36' else '')\
-              + ('\n' if next_cell_row is not current_cell_row else '')\
-              + ('------+-------+------ \n' if (next_cell_row is not current_cell_row) and \
-                                            (next_cell_row in 'DG') else ''), end='')
+def search(a_grid):
+    if all(cell['value'] is not '0' for cell in a_grid):
+        return True
+
+    try_index = 0
+
+    for cell in a_grid:
+        if cell["value"] is '0':
+            try_index = a_grid.index(cell)
+
+    print(try_index, a_grid[try_index]["index"])
+
+    for possible_value in a_grid[try_index]["possible_values"]:
+        if is_safe(a_grid, try_index, possible_value):
+            a_grid[try_index]["value"] = possible_value
+
+            if search(a_grid):
+                return True
+
+            a_grid[try_index]["value"] = '0'
+
+    return False
 
 
 main()
